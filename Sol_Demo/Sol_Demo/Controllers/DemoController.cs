38af0b62781f1a2313b5756f40e441519dc6c85e
KishorNaik <kishor.naik011.net@gmail.com>
2020-01-27 16:21:55 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Controllers/DemoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sol_Demo.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            ViewBag.Data = HttpContext.Items["SecretKey"];
+
+            return View();
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Middlewares/ContextMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Sol_Demo.Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ContextMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ContextMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            httpContext.Items.Add("SecretKey", Guid.NewGuid().ToString());
+
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ContextMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseContextMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ContextMiddleware>();
+        }
+    }
+}
